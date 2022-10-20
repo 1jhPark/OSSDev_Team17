ea8ffc509cfe00945441a57a2392696a74c9cfc7
Jaehyun <wogus4512@naver.com>
2022-10-20 18:20:40 +0900

    Unity main setting
    유니티 메인 세팅

---
+++ Flappy Bird Mobile/Assets/GameScripts/BirdBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//플레이어 캐릭터(새)의 행동을 제어하는 코드. 
+//조작법은 터치 한 번에 점프 한 번. 
+//컴퓨터 테스트 버전을 위해 마우스 클릭도 가능하게 해야 함
+
+public class BirdBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
